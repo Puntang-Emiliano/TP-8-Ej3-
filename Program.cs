88a8a570af5e51d925b4de7b7256ba42444ab2f8
Puntang Emiliano <Puntang.emiliano@gmail.com>
2022-11-10 18:53:26 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿/*3) Desarrollar un programa que solicite al usuario ingresar 6 números y almacenar en ARRAY y
+por último se lo debe recorrer para calcular y mostrar el promedio de los valores.*/
+
+
+using System;
+
+namespace Ej3_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            float[] numerooos;
+            numerooos = new float[6];
+     
+
+            float acum = 0, promedio = 0;
+            Console.Clear();
+
+            for (int i = 0; i < numerooos.Length ; i++)
+            {
+                Console.WriteLine("ingrese el numero de la posicion:" + " " + (i + 1));
+                numerooos[i] = Convert.ToSingle(Console.ReadLine());
+            }
+
+            for (int i = 0; i < numerooos.Length ; i++)
+            {
+                Console.WriteLine("El NUMERO INGRESADO EN LA POSICION   " + (i + 1) + " ES: " + numerooos[i]);
+                acum = acum + numerooos[i];
+            }
+            promedio = acum / numerooos.Length;
+            Console.WriteLine("El rpomedio de los numeros es: " + promedio);
+
+        }
+    }
+
+
+
+
+
+
+}
+
